e0aa2786af5329f8a232ef7a671acebe5a6247ef
Gael-de-Sailly <gael.chretien@akeonet.com>
2018-01-08 22:18:14 +0100

    Logs and various bugfixes

--- init.lua
+++ init.lua
@@ -7,7 +7,6 @@ local remove_delay = 10
 
 local function parse(str, signed) -- little endian
 	local bytes = {str:byte(1, -1)}
-	local count = #bytes
 	local n = 0
 	local byte_val = 1
 	for _, byte in ipairs(bytes) do
@@ -56,16 +55,27 @@ minetest.register_on_mapgen_init(function(mgparams)
 	minetest.set_mapgen_params({mgname="singlenode", flags="nolight"})
 end)
 
+local mapgens = 0
+local time_sum = 0
+local time_sum2 = 0
+
+local function displaytime(time)
+	return math.floor(time * 1000000 + 0.5) / 1000 .. " ms"
+end
+
 minetest.register_on_generated(function(minp, maxp, seed)
+	print("[imgen] Generating from " .. minetest.pos_to_string(minp) .. " to " .. minetest.pos_to_string(maxp))
+	local t0 = os.clock()
+
 	local c_stone = minetest.get_content_id("default:stone")
 	local c_dirt = minetest.get_content_id("default:dirt")
 	local c_lawn = minetest.get_content_id("default:dirt_with_grass")
 	local c_water = minetest.get_content_id("default:water_source")
 
-	xmin = math.max(minp.x, 0)
-	xmax = math.min(maxp.x, X)
-	zmin = math.max(minp.z, -Z) -- Reverse Z coordinates
-	zmax = math.min(maxp.z, 0)
+	local xmin = math.max(minp.x, 0)
+	local xmax = math.min(maxp.x, X)
+	local zmin = math.max(minp.z, -Z) -- Reverse Z coordinates
+	local zmax = math.min(maxp.z, 0)
 
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	vm:get_data(data)
@@ -78,6 +88,9 @@ minetest.register_on_generated(function(minp, maxp, seed)
 
 		local nchunk = math.floor(x / frag) + math.floor(-z / frag) * chunks_x + 1
 		if not chunks[nchunk] then
+			print("[imgen]   Loading chunk " .. nchunk)
+			local t1 = os.clock()
+
 			local address_min = index[nchunk-1] -- inclusive
 			local address_max = index[nchunk] -- exclusive
 			local count = address_max - address_min
@@ -85,6 +98,9 @@ minetest.register_on_generated(function(minp, maxp, seed)
 			local data_raw = minetest.decompress(file:read(count))
 			chunks[nchunk] = data_raw
 			chunks_delay[nchunk] = remove_delay
+			
+			local t2 = os.clock()
+			print("[imgen]   Loaded chunk " .. nchunk .. " in " .. displaytime(t2-t1))
 		end
 
 		local xpx = x % frag
@@ -93,7 +109,7 @@ minetest.register_on_generated(function(minp, maxp, seed)
 
 		local h = math.floor(parse(chunks[nchunk]:sub((npx-1)*itemsize + 1, npx*itemsize), signed) / vertical_ratio)
 
-		for y = minp.y, math.max(h, 0) do
+		for y = minp.y, math.min(math.max(h, 0), maxp.y) do
 			local node
 			if h - y < 3 then
 				if h == y and y >= 0 then
@@ -118,6 +134,7 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	vm:update_liquids()
 	vm:write_to_map()
 
+	local chunk_count = 0
 	-- Decrease delay, remove chunks from cache when time is over
 	for n, delay in pairs(chunks_delay) do
 		if delay <= 1 then
@@ -125,6 +142,24 @@ minetest.register_on_generated(function(minp, maxp, seed)
 			chunks_delay[n] = nil
 		else
 			chunks_delay[n] = delay - 1
+			chunk_count = chunk_count + 1
 		end
 	end
+
+	local t3 = os.clock()
+	local time = t3 - t0
+	print("[imgen] Mapgen finished in " .. displaytime(time))
+	print("[imgen] Cached chunks: " .. chunk_count)
+
+	mapgens = mapgens + 1
+	time_sum = time_sum + time
+	time_sum2 = time_sum2 + time ^ 2
+end)
+
+minetest.register_on_shutdown(function()
+	print("[imgen] Mapgen calls: " .. mapgens)
+	local average = time_sum / mapgens
+	print("[imgen] Average time: " .. displaytime(average))
+	local stdev = math.sqrt(time_sum2 / mapgens - average ^ 2)
+	print("[imgen] Standard dev: " .. displaytime(stdev))
 end)
